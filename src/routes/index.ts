10dd0a307181e33f06500e86e15b262711aa4876
DiogoWernek <diogowernek86@gmail.com>
2025-03-20 14:38:47 -0300

    Feito rotas de tables

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express"
 
 import { productsRoutes } from "./productsRoutes"
+import { tablesRoutes } from "./tableRoutes"
 
 const routes = Router()
 routes.use("/products", productsRoutes)
+routes.use("/tables", tablesRoutes)
 
 export { routes }
